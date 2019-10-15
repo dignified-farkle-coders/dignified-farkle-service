6dd57b3dbe5a254cf22ef50529c727aa38825fad
Rlong1218 <7dbrobert1218@gmail.com>
2019-10-15 16:39:55 -0600

    initial commit.

---
+++ src/main/java/io/farkle/dignifiedfarkleservice/DignifiedFarkleServiceApplication.java
@@ -0,0 +1,13 @@
+package io.farkle.dignifiedfarkleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DignifiedFarkleServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DignifiedFarkleServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/farkle/dignifiedfarkleservice/DignifiedFarkleServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.farkle.dignifiedfarkleservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DignifiedFarkleServiceApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}

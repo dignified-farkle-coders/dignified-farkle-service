c83d1b409587f8fcf8cc0229fb96c2115e1cd752
James Larivee <mr_15th_entertainer@yahoo.com>
2019-10-16 16:48:01 -0600

    Returns die ready to be  tallied.

--- src/main/java/io/farkle/dignifiedfarkleservice/KeepRoll.java
+++ src/main/java/io/farkle/dignifiedfarkleservice/KeepRoll.java
@@ -1,5 +1,26 @@
 package io.farkle.dignifiedfarkleservice;
 
+import java.util.ArrayList;
+
 public class KeepRoll {
 
+  public static int keepers(int[] receiveDice) {
+      
+    int[] keeps = new int[]{1,1,5,5,5,4};
+    return keeps[];
+  }
+
+  public static int rerollers(int[] receiveDice) {
+
+    return 5;
+  }
+
+  public static void main(String[] args) {
+
+
+    KeepRoll.keepers(new int[]{1,3,4,2,6,5});
+  }
+
 }
+
+

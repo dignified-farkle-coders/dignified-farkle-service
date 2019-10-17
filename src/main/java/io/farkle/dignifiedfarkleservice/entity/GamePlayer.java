116748ccf0f8c7cbfdbc6b83110a8c4bfaeb5960
James Larivee <mr_15th_entertainer@yahoo.com>
2019-10-17 16:13:37 -0600

    Wrote Entity for GamePlayer

--- src/main/java/io/farkle/dignifiedfarkleservice/entity/GamePlayer.java
+++ src/main/java/io/farkle/dignifiedfarkleservice/entity/GamePlayer.java
@@ -1,5 +1,80 @@
 package io.farkle.dignifiedfarkleservice.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(
+    uniqueConstraints = @UniqueConstraint(columnNames = {"game_id", "player_id", "join_time", "left_time"}),
+        indexes = {
+            @Index(columnList = "game_id, player_id"),
+            @Index(columnList = "order, points")
+    }
+)
 public class GamePlayer {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "game_player_id", updatable = false, nullable = false)
+  private Long id;
+
+  @NonNull
+  @ManyToOne(fetch = FetchType.EAGER, optional = false)
+  @JoinColumn(name = "game_id", nullable = false, updatable = false)
+  private Game game;
+
+  @NonNull
+  @ManyToOne(fetch = FetchType.EAGER, optional = false)
+  @JoinColumn(name = "player_id", nullable = false, updatable = false)
+  private Player player;
+
+  private int order;
+
+  private int points;
+
+  public Long getId() {
+    return id;
+  }
+
+  public Game getGame() {
+    return game;
+  }
+
+  public void setGame(Game game) {
+    this.game = game;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
+
+  public int getOrder() {
+    return order;
+  }
+
+  public void setOrder(int order) {
+    this.order = order;
+  }
+
+  public int getPoints() {
+    return points;
+  }
+
+  public void setPoints(int points) {
+    this.points = points;
+  }
 }

7dc6a765b02af4288445aa5ec50e14cc703ccbe4
T1elah <ta12b@my.fsu.edu>
2019-10-18 18:39:19 -0600

    Pull

--- src/main/java/io/farkle/dignifiedfarkleservice/model/Roll.java
+++ src/main/java/io/farkle/dignifiedfarkleservice/model/Roll.java
@@ -3,25 +3,25 @@ package io.farkle.dignifiedfarkleservice.model;
 import java.util.Arrays;
 import java.util.Random;
 
+
 public class Roll {
 
   static Random rng = new Random();
+
+
   // Request from 1-6 return the values of the dice.
 
   public static int[] rollDice(int generateDiceNumber){
     // create array for rolled dice values
-
+    int myRandomNumber;
     int [] receiveDice = new int[generateDiceNumber];
 
-    for (int i = 1; i < receiveDice.length; i++ ){
-
-      receiveDice[i] = rng.nextInt(5)+1;
-
+    for (int i = 0; i < generateDiceNumber; i++) {
+      myRandomNumber = rng.nextInt(6) + 1;
+      receiveDice[i] = myRandomNumber;
     }
 
     return receiveDice;
-
-
   }
 
 }

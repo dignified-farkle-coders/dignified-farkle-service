511589d697883793e7e11187ea8e20f387460c37
T1elah <ta12b@my.fsu.edu>
2019-10-21 08:10:50 -0600

    Updated Main

--- src/main/java/io/farkle/dignifiedfarkleservice/model/Main.java
+++ src/main/java/io/farkle/dignifiedfarkleservice/model/Main.java
@@ -8,12 +8,14 @@ public class Main {
   public static void main(String[] args) {
     int startingDice = 6;
 
+
+    //Accepts Players.
     Scanner scanner = new Scanner(System.in);
     System.out.println("How Many Players?");
     int numberOfPlayers = Integer.parseInt(scanner.nextLine());
     Player[] players = new Player[numberOfPlayers];
 
-    // initialization, set the player array full of player objects
+    // initialization, set the player array full of player objects.
     for (int i = 0; i < players.length; i++) {
       System.out.printf("Who is player number %d?%n", i + 1);
       Player player = new Player();
@@ -24,6 +26,7 @@ public class Main {
     System.out.println(Arrays.toString(players));
     System.out.println("First roll: \n");
 
+    //Displays current dice.
     System.out.println(Choice.remainingDice(Roll.rollDice(6)));
     System.out.println(PointTally.pointTally(Choice.getKeepers()));
     System.out.println("These are your new dice: " + Arrays.toString(Roll.rollDice(Choice.getReroll())));
